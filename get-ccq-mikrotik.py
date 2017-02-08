db3930b672d29ebe32cd6efd8b9e924065df696c
dionialves <dionialves@gmail.com>
2017-02-07 23:18:44 -0200

    Commit #05

--- get-ccq-mikrotik.py
+++ get-ccq-mikrotik.py
@@ -1,83 +1,88 @@
-#!/usr/bin/python
 # -*- encoding: utf-8 -*-
+# !/usr/bin/python
 import paramiko
+import re
+import json
 import sys
 
 
-class ConexaoSSH(object):
+class ConnectionSSH(object):
 
-    def __init__(self, host, usuario, senha):
+    def __init__(self, host, user, password):
         self.host = host
-        self.usuario = usuario
-        self.senha = senha
+        self.user = user
+        self.password = password
 
-    def conectar(self):
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    def connection(self):
+        connect = paramiko.SSHClient()
+        connect.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
         try:
-            # Conecta ao servidor
-            ssh.connect(self.host, username=self.usuario, password=self.senha)
-            return ssh
+            # Connect to server
+            connect.connect(self.host, username=self.user, password=self.password)
+            return connect
 
-        except ValueError:
-            return "Problemas com a conexão"
+        except:
+            print("UKNOWN - Authentication failed")
+            sys.exit(3)
 
-    def executarComando(self, comando):
+    def execute_command(self, command):
 
-        ssh = self.conectar()
+        connect = self.connection()
 
-        # Executa linha de comando
-        stdin, stdout, stderr = ssh.exec_command(comando)
-        resultado = stdout.readlines()
-        ssh.close()
+        # Run command line
+        stdin, stdout, stderr = connect.exec_command(command)
+        result = stdout.readlines()
+        connect.close()
 
-        return resultado
+        return result
 
 if __name__ == '__main__':
-    # Variaveis passadas na chamado do script
-    host = sys.argv[1]
-    usuario = sys.argv[2]
-    senha = sys.argv[3]
-    critical = sys.argv[4]
-    warning = sys.argv[5]
 
-    # Inicia objeto da classe
-    ssh = ConexaoSSH(host, usuario, senha)
-    # Pega resultado da linha
-    string = ssh.executarComando('/interface wireless registration-table print stats without-paging')
+    # Variables received via the command line
+    argv_host = sys.argv[1]
+    argv_user = sys.argv[2]
+    argv_password = sys.argv[3]
+    argv_critical = sys.argv[4]
+    argv_warning = sys.argv[5]
 
-    # Trata string para obter os resultados de CCQ, tx e rx
-    string = str(string)
-    if string:
-        tx = string.find('tx-ccq')
-        rx = string.find('rx-ccq')
+    # Start the class object
+    conn = ConnectionSSH(argv_host, argv_user, argv_password)
 
-        tx=string[tx+7:tx+10]
-        rx=string[rx+7:rx+10]
+    # Get result line
+    result_list = str(conn.execute_command('/interface wireless registration-table print stats without-paging')).split(" ")
 
-        if tx[2] == '%':
-            tx = tx[0:2]
+    # In the code below, we extract the values ​​of tx and rx and put them in a dictionary.
+    try:
+        new_list = []
+        for x in result_list:
+            if (re.search("tx-ccq", x) is not None) or (re.search("rx-ccq", x) is not None):
+                new_element = x.replace("=", '":"').replace("tx", '"tx').replace("rx", '"rx').replace("%", '%"')
+                new_list.append(new_element)
 
-        if rx[2] == '%':
-            rx = rx[0:2]
+        to_string = ",".join(str(x) for x in new_list)
+        to_string = "{" + to_string + "}"
+        to_dict = json.loads(to_string)
 
-    else:
-        print "UKNOWN - Dados indisponiveis"
+        # To validate the CCQ status, we remove the '%'.
+        tx = to_dict['tx-ccq'].replace('%', '')
+        rx = to_dict['rx-ccq'].replace('%', '')
+
+    except:
+        print("UKNOWN - Incompatible equipment")
         sys.exit(3)
 
-
-    # Devolve resultado ao nagios
-    if (int(tx) >= int(warning)) and (int(rx)>= int(warning)):
-        print "OK - Qualidade de CCQ = %s/%s" % (tx, rx)
+    # Returns result to nagios
+    if (int(tx) >= int(argv_warning)) and (int(rx) >= int(argv_warning)):
+        print("OK - Client Connection Quality (CCQ) = %s/%s" % (to_dict['tx-ccq'], to_dict['rx-ccq']))
         sys.exit(0)
-    elif (int(tx) >= int(critical)) and (int(tx) < int(warning)) or \
-         (int(rx) >= int(critical)) and (int(rx) < int(warning)):
-        print "WARNING - Qualidade de CCQ = %s/%s" % (tx, rx)
+    elif (int(tx) >= int(argv_critical)) and (int(tx) < int(argv_warning)) or \
+         (int(rx) >= int(argv_critical)) and (int(rx) < int(argv_warning)):
+        print("WARNING - Client Connection Quality (CCQ) = %s/%s" % (to_dict['tx-ccq'], to_dict['rx-ccq']))
         sys.exit(1)
-    elif (int(tx) < int(critical)) or (int(rx) < int(critical)):
-        print "CRITICAL - Qualidade de CCQ = %s/%s" % (tx, rx)
+    elif (int(tx) < int(argv_critical)) or (int(rx) < int(argv_critical)):
+        print("CRITICAL - Client Connection Quality (CCQ) = %s/%s" % (to_dict['tx-ccq'], to_dict['rx-ccq']))
         sys.exit(2)
     else:
-        print "UKNOWN - Dados indisponiveis"
+        print("UKNOWN - Unavailable data")
         sys.exit(3)
